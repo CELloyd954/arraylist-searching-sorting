b12b39885df07764bb06e2249304a76e9ccf1891
CELloyd954 <edward2020lloyd@gmail.com>
2020-04-12 16:14:20 -0600

    finished binary search, all done

--- Search.java
+++ Search.java
@@ -11,10 +11,35 @@ public class Search {
 		return found;
 	}
 
-	//public static int binarySearch(ArrayList<String> strList, String str) {
+	public static int binarySearch(ArrayList<String> strList, String str) {
 	// 	compare first letter of strings to see which half it's on??
+		int i = -1;
+		int end = strList.size() - 1;
+		int start = 0;
+		while (start < end && i == -1) {
+			int mid = (start + end) / 2;
 
-	// }
+			if (strList.get(mid).equals(str)) {
+				i = mid;
+			} else if (str.compareTo(strList.get(mid)) > 0) {
+				start = mid;
+			} else {
+				end = mid;
+			}
+
+			if (end - start <= 1) {
+				if (end == strList.size() - 1) {
+					i = strList.size() - 1;
+				}
+			} else {
+				if (strList.get(0).equals(str)) {
+					i = 0;
+				}
+			}
+			end = start;
+		}
+		return i;
+	}
 
 	public static void main(String[] args) {
 		ArrayList<String> strList = new ArrayList<String>();
@@ -27,6 +52,7 @@ public class Search {
 		strList.add("blueberry");
 		strList.add("strawberry");
 
-		System.out.println(Searching(strList, "acai"));
+		// System.out.println(Searching(strList, "acai"));
+		System.out.println(binarySearch(strList, "acai"));
 	}
 }
